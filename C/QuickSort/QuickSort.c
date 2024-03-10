6b4c1267b688e9485c8df0a028448b3273c456d1
clib <vitor.carvalho.ufu@gmail.com>
2024-03-10 19:00:25 -0300

    Quicksort in C + New Test Cases

--- C/QuickSort/QuickSort.c
+++ C/QuickSort/QuickSort.c
@@ -13,22 +13,30 @@ int main(){
 	int arr2[] = {12, 17, 8, 2, 4, 9, -1, -2, 0, 19, 12};
 	int arr3[] = {45, 46, 47};
 	int arr4[] = {337};
+	int arr5[] = {10, 9, 8, 7, 6, 5};
+	int arr6[] = {-1, -2};
 
 	PrintArray(arr, NELEMEN(arr));
 	PrintArray(arr2, NELEMEN(arr2));
 	PrintArray(arr3, NELEMEN(arr3));
 	PrintArray(arr4, NELEMEN(arr4));
+	PrintArray(arr5, NELEMEN(arr5));
+	PrintArray(arr6, NELEMEN(arr6));
 	
 	QuickSort(arr, 0, NELEMEN(arr)-1);
 	QuickSort(arr2, 0, NELEMEN(arr2)-1);
 	QuickSort(arr3, 0, NELEMEN(arr3)-1);
 	QuickSort(arr4, 0, NELEMEN(arr4)-1);
+	QuickSort(arr5, 0, NELEMEN(arr5)-1);
+	QuickSort(arr6, 0, NELEMEN(arr6)-1);
 	
 	printf("\nSorted Arrays:\n");
 	PrintArray(arr, NELEMEN(arr));
 	PrintArray(arr2, NELEMEN(arr2));
 	PrintArray(arr3, NELEMEN(arr3));
 	PrintArray(arr4, NELEMEN(arr4));
+	PrintArray(arr5, NELEMEN(arr5));
+	PrintArray(arr6, NELEMEN(arr6));
 
 	return 0;
 }
@@ -52,12 +60,16 @@ int Partitioning(int *arr, int start, int pivot){
 	int i = start;
 	while(j<pivot){
 		if(arr[j]<arr[pivot]){
-			swap(arr, i, j);
+			if(i!=j){
+				swap(arr, i, j);
+			}
 			i++;
 		}
 		j++;
 	}
-	swap(arr, i, pivot);
+	if(pivot!=i){
+		swap(arr, i, pivot);
+	}
 	return i;
 }
 
